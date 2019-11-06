e864a1350faf089f833cfca5d3d146d7910da499
Student <example@example.com>
2019-11-06 15:26:36 -0800

    update the javascript and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,54 @@
-//player one
 var playerOneBank = []
 var playerOneTurnBank = []
-//when rolled
 
+function diceRoll() {
+  return Math.floor(Math.random() * Math.floor(6))+1;
+}
+
+var accumulatorVar = (accumulator, currentValue) => accumulator + currentValue;
+
+
+
+
+//player one
 $(document).ready(function(){
   $("#playerOneRollBtn").click(function(){
-    function diceRoll() {
-      return Math.floor(Math.random() * Math.floor(6))+1;
-    }
     var diceValue = diceRoll();
-    var accumulatorVar = (accumulator, currentValue) => accumulator + currentValue;
-    //Dice Logic
+    //display num
+    $("#diceGen").text(diceValue);
     if (diceValue === 1) {
-      playerOneTurnBank = []
+      playerOneTurnBank = [0]
+      $("#pOneTurnBank").text(playerOneTurnBank);
       console.log("got a one and cleared the array");
+      console.log(playerOneTurnBank);
+      //clear turnbank
     }
     else {
       playerOneTurnBank.push(diceValue);
       playerOneTurnBank.reduce(accumulatorVar);
       var result = playerOneTurnBank.reduce(accumulatorVar);
-        playerOneTurnBank = []
+      playerOneTurnBank = []
       playerOneTurnBank.push(result);
+      $("#pOneTurnBank").text(playerOneTurnBank);
       console.log(diceValue);
       console.log(playerOneTurnBank);
     }
+    console.log("test");
+
+  });
+
+  $("#PlayerOneHoldBtn").click(function(){
+    playerOneBank.push(playerOneTurnBank[0])
+    console.log(playerOneBank);
+    playerOneBank.reduce(accumulatorVar);
+    var resultForBank = playerOneBank.reduce(accumulatorVar);
+    playerOneBank = []
+    playerOneBank.push(resultForBank);
+    playerOneTurnBank = []
+    console.log(playerOneBank)
+      $("#pOneBank").text(playerOneBank);
+
+
   });
 });
 //player two
